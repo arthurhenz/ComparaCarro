89040f22cf8d99114053fb6b70032fd9fd962237
Arthur Henrique Henz <arthur.henz@edu.pucrs.br>
2026-04-02 15:44:22 -0300

    Refactor AndroidApplicationConventionPlugin.kt

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -1,5 +1,5 @@
 import com.android.build.api.dsl.ApplicationExtension
-import com.comparacarro.configureKotlinAndroid
+import extensions.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
