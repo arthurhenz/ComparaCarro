fc312fc015a9bdf8c11818c9c3c30ffcd8c00021
Arthur Henrique Henz <arthur.henz@edu.pucrs.br>
2026-04-09 13:02:43 -0300

    Remove rememberKoinModules from Routes

--- feature/comparison/src/main/java/com/selectCompare/SelectComparisonRoute.kt
+++ feature/comparison/src/main/java/com/selectCompare/SelectComparisonRoute.kt
@@ -2,7 +2,6 @@ package com.selectCompare
 
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.common.navigation.NavOptions
@@ -11,17 +10,11 @@ import com.navigation.routes.CompareScreenRoute
 import com.navigation.routes.HomeScreenRoute
 import com.navigation.routes.SelectComparisonRoute
 import com.navigation.routes.navigateToDetail
-import org.koin.compose.module.rememberKoinModules
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
-import org.koin.ksp.generated.module
 
 fun EntryProviderScope<NavKey>.selectComparisonRoute() {
     entry<SelectComparisonRoute> { key ->
-        rememberKoinModules(unloadOnForgotten = true, unloadOnAbandoned = true) {
-            listOf(SelectComparisonModule().module)
-        }
-
         val viewModel: SelectComparisonViewModel = koinViewModel { parametersOf(key.firstId ?: "") }
 
         val state by viewModel.state.collectAsState()
