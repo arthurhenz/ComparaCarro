1eb628bf29b1c09d31e505c5da69094835436c0d
Arthur Henrique Henz <arthur.henz@edu.pucrs.br>
2026-04-09 13:08:09 -0300

    Remove libs dependeicies from core/data

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("comparacarro.android.library")
+    alias(libs.plugins.app.library)
     id("comparacarro.koin")
     id("comparacarro.detekt")
 }
@@ -9,17 +9,11 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
 
     implementation(project(":core:common"))
     implementation(project(":network"))
 
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.koin.compose.viewmodel)
     implementation(libs.androidx.datastore.preferences)
 }
