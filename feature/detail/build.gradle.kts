d1e3460b0cd0bcfb61d12664dfd241487898e41f
Arthur Henrique Henz <arthur.henz@edu.pucrs.br>
2026-04-09 13:12:15 -0300

    Remove dependencies from feature/detail

--- feature/detail/build.gradle.kts
+++ feature/detail/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("comparacarro.android.library")
+    alias(libs.plugins.app.library)
     id("comparacarro.android.compose")
     id("comparacarro.koin")
     id("comparacarro.detekt")
@@ -10,24 +10,8 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(kotlin("test"))
-
-    implementation(libs.androidx.navigation3.ui)
-    implementation(libs.androidx.navigation3.runtime)
-    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
-
     implementation(project(":core:ui"))
     implementation(project(":core:data"))
     implementation(project(":core:common"))
     implementation(project(":core:navigation"))
-
-    implementation(libs.koin.compose)
-    implementation(libs.koin.compose.viewmodel)
 }
